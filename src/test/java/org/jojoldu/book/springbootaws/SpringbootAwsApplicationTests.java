5c348b5a3e001c6efb3e4af5f5cd8eb984871739
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-02-28 21:34:10 +0900

    - 기본 설정 commit

---
+++ src/test/java/org/jojoldu/book/springbootaws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jojoldu.book.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
