0b557a566d8ee8f06c840875d188315b2071b82f
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-04 23:38:58 +0900

    -게시물 삭제 및 수정기능 구현 -추후 머스테치 템플릿을 타임리프 템플릿으로 리팩터링 예정

--- src/main/java/org/jojoldu/book/springbootaws/web/IndexController.java
+++ src/main/java/org/jojoldu/book/springbootaws/web/IndexController.java
@@ -1,10 +1,12 @@
 package org.jojoldu.book.springbootaws.web;
 
 import lombok.RequiredArgsConstructor;
+import org.jojoldu.book.springbootaws.web.dto.PostsResponseDto;
 import org.jojoldu.book.springbootaws.web.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,6 +22,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable("id") Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 
 }
--- src/main/java/org/jojoldu/book/springbootaws/web/PostsApiController.java
+++ src/main/java/org/jojoldu/book/springbootaws/web/PostsApiController.java
@@ -2,11 +2,14 @@ package org.jojoldu.book.springbootaws.web;
 
 
 import lombok.RequiredArgsConstructor;
+import org.jojoldu.book.springbootaws.web.dto.PostsListResponseDto;
 import org.jojoldu.book.springbootaws.web.dto.PostsResponseDto;
 import org.jojoldu.book.springbootaws.web.dto.PostsSaveRequestDto;
 import org.jojoldu.book.springbootaws.web.dto.PostsUpdateRequestDto;
 import org.jojoldu.book.springbootaws.web.service.posts.PostsService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,6 +30,17 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable("id") Long id){
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable("id") Long id){
+        postsService.delete(id);
+        return id;
     }
 
 }
