5b6b908dd7accbe619a8cf55740d2155272efbf6
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-03-10 21:11:04 +0900

    - kill CURRENT_PID가 작동하지 않는 에러 해결 (grep jar -> grep java)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,9 @@ PROJECT_NAME=springbootAWS
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동 중인 애플리케이션ci kill 안됨 pid 확인"
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션pid: $CURRENT_PID"
 
